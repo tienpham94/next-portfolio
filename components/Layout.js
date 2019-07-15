a047991bdc9832628d6647e63026a57e78965825
Tien Pham <tiendev94@gmail.com>
2019-07-15 22:23:54 +0300

    _document

--- components/Layout.js
+++ components/Layout.js
@@ -15,10 +15,6 @@ export default ({ children, title }) => (
   <div className="root">
     <Head>
       <title>NextPortfolio</title>
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
-      />
     </Head>
     <header>
       <Link href="/">
